65242cd08f40e6748573f252cddd679fbc9ea31b
Jonmari <jonmari@simplecloudology.com>
2022-01-14 23:04:46 +0800

    sidebar social media

---
+++ src/components/PinDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PinDetail = () => {
+    return <div>pindetails</div>;
+};
+
+export default PinDetail;
--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes } from "react-router-dom";
 
-const Pins = () => {
-    return <div>Pins</div>;
+import { Navbar, Feed, PinDetail, Search, CreatePin } from "../components";
+
+const Pins = ({ user }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+        <div className="px-2 md:px-5">
+            <div className="bg-gray-50">
+                <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+            <div className="h-full">
+                <Routes path="/" element={<Feed />} />
+                <Routes path="/categpry/:categoryId" element={<Feed />} />
+                <Routes
+                    path="/pin-detail/:pinId"
+                    element={<PinDetail user={user} />}
+                />
+                <Routes
+                    path="/create-pin"
+                    element={<CreatePin user={user} />}
+                />
+                <Routes
+                    path="/search"
+                    element={
+                        <Search
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
+                        />
+                    }
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Pins;
