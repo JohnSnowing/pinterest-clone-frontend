c20b3de7d70ea9a6fcb4e73bd24ee804426c39da
Jonmari <jonmari@simplecloudology.com>
2022-01-19 01:34:59 +0800

    pins download image

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Navbar, Feed, PinDetail, Search, CreatePin } from "../components";
 
@@ -8,28 +8,34 @@ const Pins = ({ user }) => {
     return (
         <div className="px-2 md:px-5">
             <div className="bg-gray-50">
-                <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <Navbar
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    user={user}
+                />
             </div>
             <div className="h-full">
-                <Routes path="/" element={<Feed />} />
-                <Routes path="/categpry/:categoryId" element={<Feed />} />
-                <Routes
-                    path="/pin-detail/:pinId"
-                    element={<PinDetail user={user} />}
-                />
-                <Routes
-                    path="/create-pin"
-                    element={<CreatePin user={user} />}
-                />
-                <Routes
-                    path="/search"
-                    element={
-                        <Search
-                            searchTerm={searchTerm}
-                            setSearchTerm={setSearchTerm}
-                        />
-                    }
-                />
+                <Routes>
+                    <Route path="/" element={<Feed />} />
+                    <Route path="/category/:categoryId" element={<Feed />} />
+                    <Route
+                        path="/pin-detail/:pinId"
+                        element={<PinDetail user={user} />}
+                    />
+                    <Route
+                        path="/create-pin"
+                        element={<CreatePin user={user} />}
+                    />
+                    <Route
+                        path="/search"
+                        element={
+                            <Search
+                                searchTerm={searchTerm}
+                                setSearchTerm={setSearchTerm}
+                            />
+                        }
+                    />
+                </Routes>
             </div>
         </div>
     );
