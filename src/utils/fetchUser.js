6c5fe9702f0a497ccc463f65a38e6b9298ca8fe1
Jonmari <jonmari@simplecloudology.com>
2022-01-28 03:32:56 +0800

    modify fetch USer

--- src/utils/fetchUser.js
+++ src/utils/fetchUser.js
@@ -1,6 +1,7 @@
 export const fetchUser = () => {
     const userInfo =
-        localStorage.getItem("user") !== undefined
+        localStorage.getItem("user") !== "undefined" ||
+        localStorage.getItem("user") !== "null"
             ? JSON.parse(localStorage.getItem("user"))
             : localStorage.clear();
 
